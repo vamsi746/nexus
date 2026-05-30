16255a4f46be2ba3f5e55c35de72499722d6a461
BCSS-Lakshmi-Narayana <lakshminarayana.in@bluecloudsoftech.com>
2026-05-30 23:04:09 +0530

    Setup Vercel experimentalServices for fullstack deploy

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
